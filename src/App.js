7a76cc7f8a68983b25a89dc65f4007362dfa64a6
Gabriel Martins <gabriel.angelo91@gmail.com>
2021-03-21 17:34:32 +0000

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This will be a french course for developers
+      </header>
+    </div>
+  );
+}
+
+export default App;
